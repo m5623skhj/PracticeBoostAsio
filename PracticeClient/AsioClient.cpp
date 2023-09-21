af5dd7b27122a7845334ade10844f1c9577af692
m5623skhj <m5623skhj@gmail.com>
2023-09-21 20:47:12 +0900

    * build fix ** 누락 수정

--- PracticeClient/AsioClient.cpp
+++ PracticeClient/AsioClient.cpp
@@ -1,11 +1,57 @@
 #include "AsioClient.h"
+#include "SerializationBuffer.h"
+#include <boost/bind.hpp>
+#include <iostream>
 
-AsioClient::AsioClient()
+AsioClient::AsioClient(boost::asio::io_service& inIO)
+	: io(inIO)
+	, socket(inIO)
 {
 
 }
 
 AsioClient::~AsioClient()
+{
+
+}
+
+void AsioClient::Connect(boost::asio::ip::tcp::endpoint& endPoint)
+{
+	socket.async_connect(endPoint,
+		boost::bind(&AsioClient::OnConnect, this, boost::asio::placeholders::error));
+}
+
+void AsioClient::OnConnect(const boost::system::error_code& errorCode)
+{
+	if (errorCode)
+	{
+		if (errorCode != boost::asio::error::eof)
+		{
+			std::cout << "Error in OnReceive() : " << errorCode.message() << std::endl;
+		}
+
+		return;
+	}
+
+	isConnected = true;
+}
+
+void AsioClient::Receive()
+{
+
+}
+
+void AsioClient::Send()
+{
+
+}
+
+void AsioClient::OnReceive()
+{
+
+}
+
+void AsioClient::OnSend()
 {
 
 }
