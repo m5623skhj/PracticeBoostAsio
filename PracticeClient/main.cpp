25633efa98dfb97cd138063e749e17bf219feb03
m5623skhj <m5623skhj@gmail.com>
2023-09-24 18:43:54 +0900

    * 클라이언트 main() 작성

--- PracticeClient/main.cpp
+++ PracticeClient/main.cpp
@@ -1,4 +1,14 @@
+#include "AsioClient.h"
+
 int main()
 {
+	boost::asio::io_service io;
+	boost::asio::ip::tcp::endpoint endPoint(boost::asio::ip::address::from_string("127.0.0.1"), PORT);
+
+	AsioClient client(io);
+	client.Connect(endPoint);
+
+	io.run();
+
 	return 0;
 }
